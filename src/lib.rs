e3d70d544e7a4858b58703892d7159dc5009d03e
Julius Rakow <julius@rakow.me>
2019-03-27 23:29:03 +0000

    remove trivial autogenerated test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,1 @@
-pub fn superoptimize(func: &mut parity_wasm::elements::FuncBody) {
-}
-
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4);
-	}
-}
+pub fn superoptimize(func: &mut parity_wasm::elements::FuncBody) {}
