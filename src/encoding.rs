46d69e23b1727e201d44a2454c2e5b1512cda76b
Julius Rakow <julius@rakow.me>
2019-04-30 16:09:59 +0100

    organize encoding file

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,94 +1,124 @@
 use parity_wasm::elements::Instruction;
-use std::convert::TryInto;
-use std::ffi::{CStr, CString};
-use std::iter::once;
-use std::ptr::{null, null_mut};
 use wassup_z3::*;
 
-fn encode_init_conditions(ctx: &Context, solver: &Solver, program: &[Instruction]) {
+struct Constants<'ctx> {
+	word_sort: Sort<'ctx>,
+	int_sort: Sort<'ctx>,
+	instruction_sort: Sort<'ctx>,
+	instruction_consts: Vec<FuncDecl<'ctx>>,
+	initial_stack: Vec<Ast<'ctx>>,
+	stack_depth: usize,
+}
+
+fn create_instruction<'ctx>(constants: &'ctx Constants, i: &Instruction) -> Ast<'ctx> {
+	use Instruction::*;
+
+	match i {
+		I32Const(_) => constants.instruction_consts[0].apply(&[]),
+		I32Add => constants.instruction_consts[1].apply(&[]),
+		_ => unimplemented!(),
+	}
+}
+
+fn create_constants(ctx: &Context, stack_depth: usize) -> Constants {
 	let word_sort = ctx.bv_sort(32);
 	let int_sort = ctx.int_sort();
-	let sort_name = ctx.string_symbol("instruction_sort");
-	let (instruction_sort, instruction_consts, instruction_testers) = ctx.enumeration_sort(
-		&ctx.string_symbol("instruction_sort"),
+	let (instruction_sort, instruction_consts, _) = ctx.enumeration_sort(
+		&ctx.string_symbol("instruction-sort"),
 		&[&ctx.string_symbol("I32Const"), &ctx.string_symbol("I32Add")],
 	);
-	let mk_instruction = |i: &Instruction| {
-		let index = match i {
-			Instruction::I32Const(_) => 0,
-			Instruction::I32Add => 1,
-			_ => unimplemented!(),
-		};
-		instruction_consts[index].apply(&[])
-	};
-
-	let stack_depth = 2;
-	let stack_vars: Vec<_> = (0..stack_depth)
+	let initial_stack: Vec<_> = (0..stack_depth)
 		.map(|i| ctx.fresh_const("initial-stack", &word_sort))
 		.collect();
 
-	// declare stack function
-	let stack_func_name = ctx.string_symbol("stack");
-
-	let stack_func_domain: Vec<_> = (0..stack_depth)
-		.map(|_| word_sort) // stack variables
-		.chain(once(int_sort)) // instruction counter
-		.chain(once(int_sort)) // stack address
-		.collect();
-	let stack_func = ctx.func_decl(&ctx.string_symbol("stack"), &stack_func_domain, &word_sort);
-
-	// set stack(xs, 0, i) == xs[i]
-	let program_counter = ctx.int(0, &int_sort);
-	for i in 0..stack_depth {
-		let stack_index = ctx.int(i, &int_sort);
-		let args: Vec<_> = stack_vars
-			.iter()
-			.cloned()
-			.chain(once(program_counter.clone()))
-			.chain(once(stack_index))
-			.collect();
-		let lhs = stack_func.apply(&args);
-		let rhs = &stack_vars[i];
-		solver.assert(&lhs.eq(rhs));
+	Constants {
+		word_sort,
+		int_sort,
+		instruction_sort,
+		instruction_consts,
+		initial_stack,
+		stack_depth,
 	}
+}
+
+struct State<'ctx> {
+	stack_func: FuncDecl<'ctx>,
+	stack_pointer_func: FuncDecl<'ctx>,
+	program_func: FuncDecl<'ctx>,
+	program_length: usize,
+}
+
+fn create_state<'ctx>(
+	ctx: &'ctx Context,
+	constants: &'ctx Constants,
+	prefix: &str,
+	program_length: usize,
+) -> State<'ctx> {
+	let instruction_sort = constants.instruction_sort;
+
+	// declare stack function
+	let stack_func = ctx.func_decl(
+		&ctx.string_symbol(&format!("{}stack", prefix)),
+		&[
+			constants.int_sort, // instruction counter
+			constants.int_sort, // stack address
+		],
+		&constants.word_sort,
+	);
 
 	// declare stack pointer function
-	let stack_pointer_func =
-		ctx.func_decl(&ctx.string_symbol("stack_pointer"), &[int_sort], &int_sort);
-
-	// set stack_counter(0) = 0
-	solver.assert(
-		&stack_pointer_func
-			.apply(&[ctx.int(0, &int_sort)])
-			.eq(&ctx.int(0, &int_sort)),
+	let stack_pointer_func = ctx.func_decl(
+		&ctx.string_symbol(&format!("{}stack_pointer", prefix)),
+		&[constants.int_sort],
+		&constants.int_sort,
 	);
 
 	// declare program function
 	let program_func = ctx.func_decl(
-		&ctx.string_symbol("program"),
-		&[int_sort],
-		&instruction_sort,
+		&ctx.string_symbol(&format!("{}program", prefix)),
+		&[constants.int_sort],
+		&constants.instruction_sort,
 	);
-	// set program to program
-	for (index, instruction) in program.iter().enumerate() {
-		solver.assert(
-			&mk_instruction(instruction).eq(&program_func.apply(&[ctx.int(index, &int_sort)])),
-		)
+
+	State {
+		stack_func,
+		stack_pointer_func,
+		program_func,
+		program_length,
 	}
-	let program_length = program.len();
+}
 
-	// encode transition
-	for i in 0..program_length {
-		// encode stack_pointer change
-		let stack_counter_i = stack_pointer_func.apply(&[ctx.int(i, &int_sort)]);
-		let stack_counter_ii = stack_pointer_func.apply(&[ctx.int(i + 1, &int_sort)]);
+fn set_initial_state(ctx: &Context, solver: &Solver, constants: &Constants, state: &State) {
+	// set stack(0, i) == xs[i]
+	for (i, var) in constants.initial_stack.iter().enumerate() {
+		let lhs = state.stack_func.apply(&[
+			ctx.int(0, &constants.int_sort),
+			ctx.int(i, &constants.int_sort),
+		]);
+		solver.assert(&lhs.eq(var));
+	}
 
-		let (pop_count, push_count) = stack_pop_push_count(&program[i]);
-		let pop_count = ctx.int(pop_count, &int_sort);
-		let push_count = ctx.int(push_count, &int_sort);
+	// set stack_counter(0) = 0
+	solver.assert(
+		&state
+			.stack_pointer_func
+			.apply(&[ctx.int(0, &constants.int_sort)])
+			.eq(&ctx.int(0, &constants.int_sort)),
+	);
+}
 
-		let new_counter = &(&stack_counter_i - &pop_count) + &push_count;
-		solver.assert(&stack_counter_ii.eq(&new_counter));
+fn set_source_program(
+	ctx: &Context,
+	solver: &Solver,
+	constants: &Constants,
+	state: &State,
+	program: &[Instruction],
+) {
+	// set program_func to program
+	for (index, instruction) in program.iter().enumerate() {
+		let index = ctx.int(index, &constants.int_sort);
+		let instruction = create_instruction(constants, instruction);
+		solver.assert(&state.program_func.apply(&[index]).eq(&instruction))
 	}
 }
 
@@ -119,15 +149,18 @@ mod tests {
 		};
 		let solver = Solver::with_context(&ctx);
 
-		encode_init_conditions(
-			&ctx,
-			&solver,
-			&[
-				Instruction::I32Const(1),
-				Instruction::I32Const(2),
-				Instruction::I32Add,
-			],
-		);
+		let program = &[
+			Instruction::I32Const(1),
+			Instruction::I32Const(2),
+			Instruction::I32Add,
+		];
+
+		let constants = create_constants(&ctx, 2);
+		let state = create_state(&ctx, &constants, "", program.len());
+		set_initial_state(&ctx, &solver, &constants, &state);
+
+		set_source_program(&ctx, &solver, &constants, &state, program);
+
 		solver.check();
 		let model = solver.model();
 		println!("{}", model);
