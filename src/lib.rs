120e61a0059778c5c331c2227e52e3ed03f933e3
Julius Rakow <julius@rakow.me>
2019-05-29 17:34:50 +0100

    add public interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
+use crate::block::flat_blocks_mut;
+use parity_wasm::elements::Instruction;
+
 mod block;
 mod encoding;
 
-pub fn superoptimize(func: &mut parity_wasm::elements::FuncBody) {}
+pub fn superoptimize_instructions(source_program: &[Instruction]) -> Vec<Instruction> {
+	encoding::superoptimize(source_program)
+}
+
+pub fn superoptimize_func_body(func_body: &mut parity_wasm::elements::FuncBody) {
+	let code = func_body.code_mut().elements_mut();
+	let mut blocks = block::blocks(code);
+	let flat_blocks = flat_blocks_mut(&mut blocks);
+	for flat_block in flat_blocks {
+		let instructions = &flat_block[..];
+		*flat_block = superoptimize_instructions(instructions);
+	}
+	*code = block::serialize(&blocks);
+}
+
+pub fn superoptimize_module(module: &mut parity_wasm::elements::Module) {
+	if let Some(code_section) = module.code_section_mut() {
+		for func_body in code_section.bodies_mut() {
+			superoptimize_func_body(func_body);
+		}
+	}
+}
---
+++ tests/public_interface.rs
@@ -0,0 +1,24 @@
+use parity_wasm::elements::{Instruction, Module};
+
+#[test]
+fn module_consts_add() {
+	let wasm_binary = wabt::Wat2Wasm::new()
+		.convert(
+			r#"(module
+				(func (result i32)
+					i32.const 1
+					nop
+					i32.const 2
+					i32.add
+				)
+			)"#,
+		)
+		.unwrap();
+	let mut module: Module = parity_wasm::deserialize_buffer(wasm_binary.as_ref()).unwrap();
+
+	wassup::superoptimize_module(&mut module);
+
+	let func_body = &module.code_section().unwrap().bodies()[0];
+	assert_eq!(func_body.code().elements(), &[Instruction::I32Const(3)]);
+	assert!(func_body.locals().is_empty());
+}
