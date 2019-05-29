31843f3798233175bde5a63d7de3b805511bd8b5
Julius Rakow <julius@rakow.me>
2019-05-30 00:48:59 +0100

    add cli integration test

---
+++ cli/tests/integration.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod integration {
+	#[test]
+	fn simple() {
+		assert_cli::Assert::main_binary()
+			.succeeds()
+			.with_args(&["--stdin"])
+			.stdin(
+				r#"(module
+				(func (result i32)
+					i32.const 1
+					nop
+					i32.const 2
+					i32.add
+				)
+			)"#,
+			)
+			.stdout()
+			.is(r#"(module
+  (type (;0;) (func (result i32)))
+  (func (;0;) (type 0) (result i32)
+    i32.const 3))"#)
+			.unwrap();
+	}
+}
