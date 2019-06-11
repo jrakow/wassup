7b93e2f3322d648adbf1fc39870528eb2002fbdd
Julius Rakow <julius@rakow.me>
2019-06-11 17:59:42 +0100

    add criterion benchmarks

---
+++ benches/superoptimize.rs
@@ -0,0 +1,36 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use parity_wasm::elements::Instruction::*;
+use wassup::superoptimize_instructions;
+
+fn const0_add(c: &mut Criterion) {
+	c.bench_function("const0_add", |b| {
+		b.iter(|| {
+			let source = &[I32Const(0), I32Add];
+
+			superoptimize_instructions(black_box(source))
+		})
+	});
+}
+
+fn consts_add(c: &mut Criterion) {
+	c.bench_function("consts_add", |b| {
+		b.iter(|| {
+			let source = &[I32Const(1), I32Const(2), I32Add];
+
+			superoptimize_instructions(black_box(source))
+		})
+	});
+}
+
+fn const_nop(c: &mut Criterion) {
+	c.bench_function("const_nop", |b| {
+		b.iter(|| {
+			let source = &[I32Const(1), Nop];
+
+			superoptimize_instructions(black_box(source))
+		})
+	});
+}
+
+criterion_group!(benches, const0_add, consts_add, const_nop);
+criterion_main!(benches);
