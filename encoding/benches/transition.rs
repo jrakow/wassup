76e9250fdc28ee06fd572a25ecdf5d3222d66b41
Julius Rakow <julius@rakow.me>
2019-07-07 17:29:22 +0100

    add encoding benchmark

---
+++ encoding/benches/transition.rs
@@ -0,0 +1,55 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use wassup_encoding::*;
+use z3::*;
+
+fn transition_consts_add(source: &[Instruction]) {
+	let ctx = Context::new(&Config::default());
+	let solver = Solver::new(&ctx);
+
+	let constants = Constants::new(&ctx, vec![], &[]);
+	let state = State::new(&ctx, &solver, &constants, "");
+
+	state.set_source_program(source);
+
+	solver.assert(&state.transitions());
+
+	assert!(solver.check());
+	let model = solver.get_model();
+
+	let value_type = value_type(&ctx);
+	let stack = |pc, i| -> i32 {
+		let pc = &ctx.from_usize(pc);
+		let i = &ctx.from_usize(i);
+		let ast = &state.stack(pc, i);
+
+		let inner = value_type.variants[0].accessors[0].apply(&[ast]);
+		let evaled = model.eval(&inner.bv2int(true)).unwrap();
+		evaled.as_i32().unwrap()
+	};
+
+	assert_eq!(stack(1, 0), 1);
+	assert_eq!(stack(2, 0), 1);
+	assert_eq!(stack(2, 1), 2);
+	assert_eq!(stack(3, 0), 3);
+}
+
+fn consts_add(c: &mut Criterion) {
+	c.bench_function("consts_add", |b| {
+		b.iter(|| {
+			let source = &[
+				Instruction::Const(Value::I32(1)),
+				Instruction::Const(Value::I32(2)),
+				Instruction::I32Add,
+			];
+
+			transition_consts_add(black_box(source))
+		})
+	});
+}
+
+criterion_group! {
+	name = benches;
+	config = Criterion::default().sample_size(10);
+	targets = consts_add
+}
+criterion_main!(benches);
