819e1675a652232fd1568def9677724cb0be4546
Julius Rakow <julius@rakow.me>
2019-08-19 01:10:06 +0100

    enable all spec tests

--- encoding/tests/spec_tests.rs
+++ encoding/tests/spec_tests.rs
@@ -9,29 +9,210 @@ use wassup_encoding::*;
 use z3::*;
 
 #[test]
-fn test_i32() {
-	spec_test("i32.wast")
-}
+fn test_address() { spec_test("address.wast") }
 
 #[test]
-fn test_i64() {
-	spec_test("i64.wast")
-}
+fn test_align() { spec_test("align.wast") }
 
 #[test]
-fn test_select() {
-	spec_test("select.wast")
-}
+fn test_binary_leb128() { spec_test("binary-leb128.wast") }
 
 #[test]
-fn test_int_exprs() {
-	spec_test("int_exprs.wast")
-}
+fn test_block() { spec_test("block.wast") }
 
 #[test]
-fn test_conversions() {
-	spec_test("conversions.wast")
-}
+fn test_break_drop() { spec_test("break-drop.wast") }
+
+#[test]
+fn test_br_if() { spec_test("br_if.wast") }
+
+#[test]
+fn test_br_table() { spec_test("br_table.wast") }
+
+#[test]
+fn test_br() { spec_test("br.wast") }
+
+#[test]
+fn test_call_indirect() { spec_test("call_indirect.wast") }
+
+#[test]
+fn test_call() { spec_test("call.wast") }
+
+#[test]
+fn test_comments() { spec_test("comments.wast") }
+
+#[test]
+fn test_conversions() { spec_test("conversions.wast") }
+
+#[test]
+fn test_custom() { spec_test("custom.wast") }
+
+#[test]
+fn test_data() { spec_test("data.wast") }
+
+#[test]
+fn test_elem() { spec_test("elem.wast") }
+
+#[test]
+fn test_endianness() { spec_test("endianness.wast") }
+
+#[test]
+fn test_f32_bitwise() { spec_test("f32_bitwise.wast") }
+
+#[test]
+fn test_f32_cmp() { spec_test("f32_cmp.wast") }
+
+#[test]
+fn test_f32() { spec_test("f32.wast") }
+
+#[test]
+fn test_f64_bitwise() { spec_test("f64_bitwise.wast") }
+
+#[test]
+fn test_f64_cmp() { spec_test("f64_cmp.wast") }
+
+#[test]
+fn test_f64() { spec_test("f64.wast") }
+
+#[test]
+fn test_fac() { spec_test("fac.wast") }
+
+#[test]
+fn test_float_exprs() { spec_test("float_exprs.wast") }
+
+#[test]
+fn test_float_literals() { spec_test("float_literals.wast") }
+
+#[test]
+fn test_float_memory() { spec_test("float_memory.wast") }
+
+#[test]
+fn test_float_misc() { spec_test("float_misc.wast") }
+
+#[test]
+fn test_forward() { spec_test("forward.wast") }
+
+#[test]
+#[ignore]
+fn test_func_ptrs() { spec_test("func_ptrs.wast") }
+
+#[test]
+#[ignore]
+fn test_func() { spec_test("func.wast") }
+
+#[test]
+fn test_globals() { spec_test("globals.wast") }
+
+#[test]
+fn test_i32() { spec_test("i32.wast") }
+
+#[test]
+fn test_i64() { spec_test("i64.wast") }
+
+#[test]
+fn test_if() { spec_test("if.wast") }
+
+#[test]
+fn test_inline_module() { spec_test("inline-module.wast") }
+
+#[test]
+fn test_int_exprs() { spec_test("int_exprs.wast") }
+
+#[test]
+fn test_int_literals() { spec_test("int_literals.wast") }
+
+#[test]
+fn test_labels() { spec_test("labels.wast") }
+
+#[test]
+fn test_left_to_right() { spec_test("left-to-right.wast") }
+
+#[test]
+fn test_load() { spec_test("load.wast") }
+
+#[test]
+fn test_local_get() { spec_test("local_get.wast") }
+
+#[test]
+fn test_local_set() { spec_test("local_set.wast") }
+
+#[test]
+fn test_local_tee() { spec_test("local_tee.wast") }
+
+#[test]
+fn test_loop() { spec_test("loop.wast") }
+
+#[test]
+fn test_memory_grow() { spec_test("memory_grow.wast") }
+
+#[test]
+fn test_memory_redundancy() { spec_test("memory_redundancy.wast") }
+
+#[test]
+fn test_memory_size() { spec_test("memory_size.wast") }
+
+#[test]
+fn test_memory_trap() { spec_test("memory_trap.wast") }
+
+#[test]
+fn test_memory() { spec_test("memory.wast") }
+
+#[test]
+fn test_nop() { spec_test("nop.wast") }
+
+#[test]
+fn test_return() { spec_test("return.wast") }
+
+#[test]
+fn test_select() { spec_test("select.wast") }
+
+#[test]
+fn test_skip_stack_guard_page() { spec_test("skip-stack-guard-page.wast") }
+
+#[test]
+fn test_stack() { spec_test("stack.wast") }
+
+#[test]
+fn test_start() { spec_test("start.wast") }
+
+#[test]
+fn test_store() { spec_test("store.wast") }
+
+#[test]
+fn test_switch() { spec_test("switch.wast") }
+
+#[test]
+fn test_token() { spec_test("token.wast") }
+
+#[test]
+fn test_traps() { spec_test("traps.wast") }
+
+#[test]
+fn test_typecheck() { spec_test("typecheck.wast") }
+
+#[test]
+fn test_type() { spec_test("type.wast") }
+
+#[test]
+fn test_unreachable() { spec_test("unreachable.wast") }
+
+#[test]
+fn test_unreached_invalid() { spec_test("unreached-invalid.wast") }
+
+#[test]
+fn test_unwind() { spec_test("unwind.wast") }
+
+#[test]
+fn test_utf8_custom_section_id() { spec_test("utf8-custom-section-id.wast") }
+
+#[test]
+fn test_utf8_import_field() { spec_test("utf8-import-field.wast") }
+
+#[test]
+fn test_utf8_import_module() { spec_test("utf8-import-module.wast") }
+
+#[test]
+fn test_utf8_invalid_encoding() { spec_test("utf8-invalid-encoding.wast") }
 
 fn spec_test(name: &str) {
 	let source = read("tests/spec_tests/".to_owned() + name).unwrap();
